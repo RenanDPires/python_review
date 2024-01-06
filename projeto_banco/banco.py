b12c7c8cf635c855db8afa9d47d85cd08c8b98eb
RenanDPires <renan.if.210@gmail.com>
2024-01-06 20:31:41 -0300

    Banco project is done

--- projeto_banco/banco.py
+++ projeto_banco/banco.py
@@ -1,23 +1,45 @@
 from datetime import datetime
-historico_transacoes = {}
+historico_transacoes = []
 
 saldo= 0
 limite= 500
 numero_saques = 0
 LIMITE_SAQUES = 3
 
-def deposito(valor):
-    try:
-        historico_transacoes[datetime.datetime.now()] = valor
-        print(f'Depósito realizado em\t{historico_transacoes.keys()[-1]} \t no valor de {historico_transacoes.values()[-1]}')
-    except:
-        print('Para depósitos, apenas valores positivos são aceitos.')
-
 while True:
-    transacao= float(input('Informe a letra da transação desejada:\nd => Depósito\ns => Saque\ne => Extrato\n'))
+    transacao= input('Informe a letra da transação desejada:\nd => Depósito\ns => Saque\ne => Extrato\n')
     
     if transacao.upper() == 'D':
-        valor = input('Informe quanto deseja depositar\n R$ ')
-        deposito(valor)
+        valor = float(input('Informe quanto deseja depositar\n R$ '))
+        try:
+            saldo = saldo + valor
+            transacao_atual = [datetime.now().strftime("%d/%m/%Y %H:%M:%S.%f"),valor]
+            historico_transacoes.append(transacao_atual)
+            print(f'Depósito realizado em\t{transacao_atual[0]} \t no valor de R$ {transacao_atual[1]:.2f}')
+        
+        except Exception as e:
+            print('Para depósitos, apenas valores positivos são aceitos.\n',e)
 
+    elif transacao.upper() == 'S':
+        valor = float(input('Informe quanto deseja sacar\n R$ '))
+        try:
+            if (valor <= saldo) and numero_saques < 3 and valor <= 500:
+                saldo = saldo - valor
+                transacao_atual = [datetime.now().strftime("%d/%m/%Y %H:%M:%S.%f"),-valor]
+                historico_transacoes.append(transacao_atual)
+                print(f'Saque realizado em\t{transacao_atual[0]} \t no valor de R$ {transacao_atual[1]:.2f}')
 
+                numero_saques += 1
+            elif numero_saques >= 3:
+                print('Limite de saques diários excedido.')
+            elif valor > 500:
+                print('Valor de saques diários excedido.')
+            else:
+                print(f'Saldo inferior ao valor do saque.\n Saldo atual: R${saldo}')
+        except Exception as e:
+            print('Para depósitos, apenas valores positivos são aceitos.\n',e)
+
+    elif transacao.upper() == 'E':
+        for transacao in historico_transacoes:
+            print(transacao[0],'\t',"{:.2f}".format(transacao[1]))
+        print(f'Saldo final\tR$ {saldo:.2f}')
